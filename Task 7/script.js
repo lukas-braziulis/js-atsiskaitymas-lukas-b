9798f846c0668180db4b15ea80dbb82ff9ee2c4f
lukas-braziu <lukas.braziulis@gmail.com>
2022-10-12 20:09:56 +0300

    Task-07

---
+++ Task 7/script.js
@@ -0,0 +1,16 @@
+/* ------------------------------ TASK 7 -----------------------------------
+Turimas "audi" objektas.
+
+Parašykite funkciją "showObjectKeys", kuri kaip argumentą priims objektą 
+ir grąžins visus jo "key" masyve.
+-------------------------------------------------------------------------- */
+const audi = {
+  make: "audi",
+  model: "A6",
+  year: 2005,
+  color: "white",
+};
+function showObjectKeys(data) {
+  return Object.keys(data);
+}
+console.log(showObjectKeys(audi));
